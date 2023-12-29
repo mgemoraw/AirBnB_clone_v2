c59cbff3ecc2b4f3b6a340f168536ffb8230a41f
mgemoraw <meng.get4@gmail.com>
2023-12-29 11:03:43 +0300

    Hello FlasK

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,10 +7,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     """displays 'Hello HBNB!' on the web browser"""
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
