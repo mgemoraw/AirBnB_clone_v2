36b14d43fe129d48bb79ea511d0fdfcdfe63181b
mgemoraw <meng.get4@gmail.com>
2023-12-29 11:00:15 +0300

    Hello Flask

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Starting Flask Application
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    """displays 'Hello HBNB!' on the web browser"""
+    return "Hello HBNB!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
